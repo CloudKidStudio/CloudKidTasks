7d519e6cfc967450eb38d1382f6351643630d7bc
Matt Karl <matt@mattkarl.com>
2014-03-19 15:22:56 -0400

    Auto updated docs

---
+++ api.js
@@ -0,0 +1,14 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "cloudkid.FunctionTask",
+        "cloudkid.ListTask",
+        "cloudkid.LoadTask",
+        "cloudkid.Task",
+        "cloudkid.TaskEvent",
+        "cloudkid.TaskManager"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
